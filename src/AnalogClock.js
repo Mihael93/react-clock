24a75ccec241e27d9995b06dc97ef0207aef7510
Mihael93 <vynnykm93@gmail.com>
2022-09-09 19:23:25 +0300

    number style update

--- src/AnalogClock.js
+++ src/AnalogClock.js
@@ -42,6 +42,45 @@ function AnalogClock() {
             <div className="marking marking-two"></div>
             <div className="marking marking-three"></div>
             <div className="marking marking-four"></div>
+
+            <div className='clock-inner-marks'>
+               <div className="marking-small marking-small-2"></div>
+               <div className="marking-small marking-small-3"></div>
+               <div className="marking-small marking-small-4"></div>
+               <div className="marking-small marking-small-5"></div>
+
+               <div className="marking-small marking-small-6"></div>
+               <div className="marking-small marking-small-7"></div>
+               <div className="marking-small marking-small-8"></div>
+               <div className="marking-small marking-small-9"></div>
+
+               <div className="marking-small marking-small-10"></div>
+               <div className="marking-small marking-small-11"></div>
+               <div className="marking-small marking-small-12"></div>
+               <div className="marking-small marking-small-13"></div>
+
+               <div className="marking-small marking-small-14"></div>
+               <div className="marking-small marking-small-15"></div>
+               <div className="marking-small marking-small-16"></div>
+               <div className="marking-small marking-small-17"></div>
+
+               <div className="marking-small marking-small-18"></div>
+               <div className="marking-small marking-small-19"></div>
+               <div className="marking-small marking-small-20"></div>
+               <div className="marking-small marking-small-21"></div>
+
+               <div className="marking-small marking-small-22"></div>
+               <div className="marking-small marking-small-23"></div>
+               <div className="marking-small marking-small-24"></div>
+               <div className="marking-small marking-small-25"></div>
+            </div>
+
+            <div className='clock__numbers'>
+               <span className="h3">3</span>
+               <span className="h6">6</span>
+               <span className="h9">9</span>
+               <span className="h12">12</span>
+            </div>
             <div className='clock__inner-face'>
                <div className="hand hour-hand" style={{ transform: `${hours}` }} ></div>
                <div className="hand min-hand" style={{ transform: `${minutes}` }}></div>
